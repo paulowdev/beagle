13027279cd3f62524abaffe123b7c80df1354b29
carlossteinzup <carlos.stein@zup.com.br>
2020-11-25 09:49:40 -0300

    updating beff

--- backend/automated-tests/src/main/kotlin/br/com/zup/beagle/automatedtests/builders/ImageScreenBuilder.kt
+++ backend/automated-tests/src/main/kotlin/br/com/zup/beagle/automatedtests/builders/ImageScreenBuilder.kt
@@ -16,30 +16,29 @@
 
 package br.com.zup.beagle.automatedtests.builders
 
-import br.com.zup.beagle.automatedtests.constants.LOGO_BEAGLE
-import br.com.zup.beagle.automatedtests.constants.LOGO_BEAGLE_URL
-import br.com.zup.beagle.automatedtests.constants.TITLE_SCREEN
-import br.com.zup.beagle.widget.action.Alert
 import br.com.zup.beagle.widget.core.ImageContentMode
 import br.com.zup.beagle.widget.core.ScrollAxis
-import br.com.zup.beagle.widget.layout.NavigationBar
-import br.com.zup.beagle.widget.layout.NavigationBarItem
 import br.com.zup.beagle.widget.layout.Screen
 import br.com.zup.beagle.widget.layout.ScrollView
 import br.com.zup.beagle.widget.ui.Image
 import br.com.zup.beagle.widget.ui.ImagePath.Local
 import br.com.zup.beagle.widget.ui.Text
 
+const val LOGO_BEAGLE = "imageBeagle"
+const val LOGO_BEAGLE_URL = "/public/logo.png"
+
 object ImageScreenBuilder {
      fun build() = Screen(
         child = ScrollView(
             scrollDirection = ScrollAxis.VERTICAL,
-            children = listOf(createText("Image"), Image(Local.both(LOGO_BEAGLE_URL, LOGO_BEAGLE))) +
+            children = listOf(
+                createText("Image"),
+                Image(Local.both(LOGO_BEAGLE_URL, LOGO_BEAGLE))) +
                 ImageContentMode.values().flatMap(this::createImageWithModeAndText)
         )
     )
 
-    private fun createText(text: String) = Text(text = text, styleId = TITLE_SCREEN)
+    private fun createText(text: String) = Text(text = text, styleId = "Image Screen")
 
     private fun createImageWithModeAndText(mode: ImageContentMode) =
         listOf(createText("Image with contentMode = $mode"), Image(Local.both(LOGO_BEAGLE_URL, LOGO_BEAGLE), mode))
